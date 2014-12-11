e2fe58b67281a931bb3528d188ee91d69ed428de
Florian Rommel <florian.rommel@web.de>
2014-12-11 02:16:05 +0100

    added license information

---
+++ src/input.h
@@ -0,0 +1,27 @@
+/*
+ * input.h
+ * This file is part of LEDpix
+ *
+ * Copyright (C) 2014 - Florian Rommel
+ *
+ * LEDpix is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * LEDpix is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with LEDpix. If not, see <http://www.gnu.org/licenses/>.
+ */
+ 
+
+#ifndef _INPUT_H_
+#define _INPUT_H_
+
+uint16_t get_key(void);
+
+#endif
